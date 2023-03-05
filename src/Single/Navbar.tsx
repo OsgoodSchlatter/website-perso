395cbed875254dc7b943fb0fab628a83212a293f
Osgood <eloiposeidon@gmail.com>
2023-03-05 18:11:29 +0100

    adding co2 footprint for the page

--- src/Single/Navbar.tsx
+++ src/Single/Navbar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export const Navbar = () => {
   const [hoverGH, setHoverGH] = useState(false);
   const [hoverLKD, setHoverLKD] = useState(false);
+  const [hoverC, setHoverC] = useState(false);
   return (
     <>
       <header className="h-20 px-4 bg-black flex items-center overflow-hidden justify-between">
@@ -20,28 +21,68 @@ export const Navbar = () => {
               Carbon Calculator
             </Link>
           </li>
-          <li>
-            {/* <Link to="/sncf" className="text-white">
-              Sncf
-            </Link> */}
-          </li>
         </div>
-        <div className="flex bg-white gap-2 m-2 p-2 rounded-md items-end">
-          <a href={"https://www.linkedin.com/in/eloi-besnard-489a69170/"}>
+        <div className="flex items-center">
+          <div
+            onMouseOver={() => setHoverC(true)}
+            onMouseLeave={() => setHoverC(false)}
+          >
+            <div
+              className={
+                "" +
+                (hoverC
+                  ? "bg-green-500 flex gap-2 m-2 p-2 rounded-md items-end"
+                  : "bg-green-400 flex gap-2 m-2 p-2 rounded-md items-end")
+              }
+            >
+              {" "}
+              <a href="https://www.websitecarbon.com/website/osgood-on-the-web-ml/">
+                {" "}
+                0.02 gCO2eq
+              </a>
+            </div>
+          </div>
+          <div className="flex bg-white gap-2 m-2 p-2 rounded-md items-end">
             <div
               onMouseOver={() => setHoverLKD(true)}
               onMouseLeave={() => setHoverLKD(false)}
             >
-              <GrLinkedin size={hoverLKD ? 40 : 30} />
+              {hoverLKD ? (
+                <div className="bg-slate-500 rounded-lg">
+                  <a
+                    href={"https://www.linkedin.com/in/eloi-besnard-489a69170/"}
+                  >
+                    <GrLinkedin size={30} />
+                  </a>
+                </div>
+              ) : (
+                <div className=" rounded-lg">
+                  <a
+                    href={"https://www.linkedin.com/in/eloi-besnard-489a69170/"}
+                  >
+                    <GrLinkedin size={30} />
+                  </a>
+                </div>
+              )}
             </div>
-          </a>
-          <div
-            onMouseOver={() => setHoverGH(true)}
-            onMouseLeave={() => setHoverGH(false)}
-          >
-            <a href={"https://github.com/OsgoodSchlatter"}>
-              <GoMarkGithub size={hoverGH ? 40 : 30} />
-            </a>
+            <div
+              onMouseOver={() => setHoverGH(true)}
+              onMouseLeave={() => setHoverGH(false)}
+            >
+              {hoverGH ? (
+                <div className="bg-slate-500 rounded-lg">
+                  <a href={"https://github.com/OsgoodSchlatter"}>
+                    <GoMarkGithub size={30} />
+                  </a>
+                </div>
+              ) : (
+                <div className="rounded-full">
+                  <a href={"https://github.com/OsgoodSchlatter"}>
+                    <GoMarkGithub size={30} />
+                  </a>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </header>
