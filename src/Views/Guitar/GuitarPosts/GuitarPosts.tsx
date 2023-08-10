9f14d77ebc3dc753b1f2907a077e2b301b284634
Osgood <eloi.besnard@telecom-sudparis.eu>
2023-08-10 15:15:59 +0200

    guitar

---
+++ src/Views/Guitar/GuitarPosts/GuitarPosts.tsx
@@ -0,0 +1,25 @@
+import PianoSongs from "./PianoSongs";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const blogPosts = {
+  "1": {
+    id: 1,
+    date: "10/08/2023",
+    title: "Guitar Songs",
+    content: GuitarSongs,
+  },
+};
+
+export const GuitarPosts = () => {
+  const { postID } = useParams<{ postID: string }>();
+
+  const post = blogPosts[postID as keyof typeof blogPosts];
+  const Component = post ? post.content : null;
+
+  if (!Component) {
+    return <div>Loading...</div>;
+  }
+
+  return <Component postID={postID!} />;
+};
---
+++ src/Views/Guitar/GuitarPosts/GuitarSongs.tsx
@@ -0,0 +1,39 @@
+import { HeaderCategory } from "../../../Single/HeaderCategory";
+
+export const GuitarSongs = ({ postID }: { postID: string }) => {
+    return (
+        <>
+            <div className="flex justify-center ">
+                <div className="max-w-4xl">
+                    <HeaderCategory
+                        name={"List of guitar songs I play"}
+                        date={"10/08/2023"}
+                        back={`/guitar/`}
+                    />
+                    <div className="p-4 my-20 justify-center">
+                        <div>- 17 going under - Sam Fender</div>
+                        <div>- The Borders - Sam Fender</div>
+                        <div>- Spit of you - Sam Fender</div>
+                        <div>- Will we talk - Sam Fender</div>
+                        <div>- All is on my side - Sam Fender</div>
+                        <div>- Wild Grey Ocean -   Sam Fender</div>
+                        <div>- Viva la vida - Coldplay</div>
+                        <div>- Bonne idée - JJG</div>
+                        <div>- Là-bas - JJG</div>
+                        <div>- Pour que tu m'aimes encore - JJG</div>
+                        <div>- Pour que tu m'aimes encore - JJG</div>
+                        <div>- Piccola stella senza cielo - Ligabue</div>
+                        <div>- I'm yours - Jason Mraz</div>
+                        <div>- Overpass Graffiti -  Ed Sheeran</div>
+                        <div>- A Team -  Ed Sheeran</div>
+                        <div>- Need You Now - Lady A</div>
+                        <div>- J'ai demandé à la lune - Indochine</div>
+                        <div>- Let it be - the Beatles </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+
