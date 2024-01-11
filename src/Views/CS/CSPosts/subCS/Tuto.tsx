905ef3a219fbc945f0283c86b0569a8ff334cc59
Osgood <eloi.besnard@telecom-sudparis.eu>
2024-01-11 14:30:20 +0100

    add tuto

--- src/Views/CS/CSPosts/subCS/Tuto.tsx
+++ src/Views/CS/CSPosts/subCS/Tuto.tsx
@@ -6,75 +6,106 @@ const Tuto = () => {
         <div className="flex justify-center ">
             <div className="p-4 max-w-4xl">
                 <HeaderCategory name={"tuto"} date={"2024"} back={`/cs/`} />
-                <div className="font-bold text-xl">
-                    to know:
-                </div>
-                <div>
-                    <div className="font-bold">Linux</div>
-                    linux = windows but for nerd
-                </div>
-                <div>
-                    <div className="font-bold">Git</div>
-
-                    git = version manager, just like google doc enables you to edit a file, git enables you to edit code and track different version
-                </div>
-                <div>
-                    <div className="font-bold">GitHub, GitLab</div>
-
-                    gitHub, gitLab,... = websites that hosts your code online. gitHub is a website, git is a software!!
-                </div>
-                <div>
-                    <div className="font-bold">Editor, VsCode, Atom</div>
-
-                    atom, vscode = software to edit code on.
-                </div>
-                <div>
-                    <div className="font-bold">Progamming Languages</div>
-
-                    C, python, C++, java, python = different programming languages. they all have their different specificities.
-                </div>
-
-                <div>
-                    <div className="font-bold text-l">
-                        Useful commands in the terminal
-                    </div>
-                    terminal is a more direct and shorter way to navigate through your files:
-                    <div>
-                        1 - how to open terminal: CTRL + ALT + t
-                    </div>
-                    <div>
-                        2 - ls (list files) = show the files
-                    </div>
-                    <div>
-                        3 - cd (change directory) = moves through directories. cd .. moves rearward; cd ./directory moves in directory
+                <div className="mt-2">
+                    <div className="font-bold text-3xl">
+                        RESSOURCES:
                     </div>
                     <div>
 
-                        4 - mkdir (make directory) = creates a new directory
+                    </div>
+                    <a className="hover:underline" href="https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories">
+                        GitHub
+                    </a>
+                    <a href="https://docs.github.com/en/get-started/getting-started-with-git"> Getting Started with Git</a>
+
+                </div>
+                <div className="mt-2">
+
+                    <div className="font-bold text-3xl">
+                        RESUME:
                     </div>
                     <div>
-                        5 - touch your_file.cxx = creates your file
+                        <div className="font-bold">Linux</div>
+                        linux = windows but for nerd
                     </div>
                     <div>
-                        6 - run a programm:
+                        <div className="font-bold">Git</div>
+                        git = version manager, just like google doc enables you to edit a file, git enables you to edit code and track different version
+                    </div>
+                    <div>
+                        <div className="font-bold">GitHub, GitLab</div>
+
+                        gitHub, gitLab,... = websites that hosts your code online. gitHub is a website, git is a software!!
+                    </div>
+                    <div>
+                        <div className="font-bold">Editor, VsCode, Atom</div>
+
+                        atom, vscode = software to edit code on.
+                    </div>
+                    <div>
+                        <div className="font-bold">Progamming Languages</div>
+
+                        C, python, C++, java, python = different programming languages. they all have their different specificities.
+                    </div>
+
+                    <div className="mt-2">
+                        <div className="font-bold text-l">
+                            Useful commands in the terminal
+                        </div>
+                        terminal is a more direct and shorter way to navigate through your files:
                         <div>
-                            compiling: g++ -o name_of_executable name_of_file.cxx
+                            1 - how to open terminal: CTRL + ALT + t
                         </div>
                         <div>
-                            running: ./name_of_executable
+                            2 - ls (list files) = show the files
                         </div>
-                        ex: my file in c++ is physics.cxx, I compile it like so: g++ -o physics physics.cxx
-                        and execute like so ./physics
+                        <div>
+                            3 - cd (change directory) = moves through directories. cd .. moves rearward; cd ./directory moves in directory
+                        </div>
+                        <div>
 
+                            4 - mkdir (make directory) = creates a new directory
+                        </div>
+                        <div>
+                            5 - touch your_file.cxx = creates your file
+                        </div>
+                        <div>
+                            6 - run a programm:
+                            <div>
+                                compiling: g++ -o name_of_executable name_of_file.cxx
+                            </div>
+                            <div>
+                                running: ./name_of_executable
+                            </div>
+                            ex: my file in c++ is physics.cxx, I compile it like so: g++ -o physics physics.cxx
+                            and execute like so ./physics
 
-                        (compiling = the computer makes the translation from your code (physics) to binary 1010001010101.)
-                        (Executing, running = actually reading the binary and doing the computation)
+                            <div>
+                                (compiling = the computer makes the translation from your code (physics) to binary 1010001010101.)
+                                (Executing, running = actually reading the binary and doing the computation)
+                            </div>
+                        </div>
+
                     </div>
-
+                    <div className="mt-2">
+                        <div className="font-bold text-l">
+                            Useful commands for git (still in terminal)
+                        </div>
+                        <div>
+                            1 - git add your_file.cxx
+                        </div>
+                        <div>
+                            2 - git commit -m "your message"
+                        </div>
+                        <div>
+                            3 - git push origin main
+                        </div>
+                    </div>
                 </div>
 
             </div>
-        </div>
+
+        </div >
     );
 };
 
