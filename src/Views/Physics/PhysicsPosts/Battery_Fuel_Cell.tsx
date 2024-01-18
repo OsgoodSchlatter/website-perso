ad6c3c3260ccb1fd10c3eada2c86924248eded17
Osgood <eloi.besnard@telecom-sudparis.eu>
2024-01-18 13:32:11 +0100

    hydrogen

--- src/Views/Physics/PhysicsPosts/Battery_Fuel_Cell.tsx
+++ src/Views/Physics/PhysicsPosts/Battery_Fuel_Cell.tsx
@@ -3,7 +3,7 @@ import "katex/dist/katex.min.css";
 import Latex from "react-latex-next";
 import "katex/dist/katex.min.css";
 import { Tableau } from "../../../Single/Tableau";
-
+import pem from "./pem.png"
 const batteryData: (string)[][] = [
   [
     "LCO",
@@ -59,7 +59,9 @@ const batteryData: (string)[][] = [
 
 const H3PO4 = <Latex>{"$H_3PO_4$"}</Latex>;
 const KOH = <Latex>{"$KOH$"}</Latex>;
-const H2 = <Latex>{"$H2$"}</Latex>;
+const H2 = <Latex>{"$H_2$"}</Latex>;
+const O2 = <Latex>{"$O_2$"}</Latex>;
+
 const Li2CO3 = <Latex>{"$Li_2CO_3$"}</Latex>;
 const K2CO3 = <Latex>{"$K_2CO_3$"}</Latex>;
 const Na2CO3 = <Latex>{"$Na_2CO_3$"}</Latex>;
@@ -67,6 +69,8 @@ const Zr02 = <Latex>{"$ZrO_2$"}</Latex>;
 const Y2O3 = <Latex>{"$Y_2O_3$"}</Latex>;
 const CO2 = <Latex>{"$CO_2$"}</Latex>;
 const CH4 = <Latex>{"$CH_4$"}</Latex>;
+
+const reaction = <Latex>{"$2H_2 + O_2 \\rightarrow 2H_2O + heat + electrical \\ energy $"}</Latex>;
 
 const combinedLatex = [Li2CO3, K2CO3, Na2CO3];
 const combinedLatex2 = [Zr02, Y2O3];
@@ -130,6 +134,29 @@ const Battery_Fuel_Cell = () => {
           />
           <Tableau content={fuelCellData} headers={["Type", "Electrolyte", "Temp°C", "Fuel", "Efficiency", "Power", "Maturity"]} title={"Main H2 fuel cell technologies"} />
           <Tableau content={batteryData} headers={["Battery type", "Cathode", "Anode", "Cell Voltage", "Cost", "Energy Density", "Cycle Life", "Discharge Rate"]} title={"Battery: Lithium-ion technologies"} />
+          <div className="font-bold text-xl mb-3 underline">Hydrogen today: </div>
+          <div className="font-bold text-l ">Data</div>
+          <div className="mt-1 flex"> World production: <div className="font-bold px-1">75 Mt</div></div>
+          <div className="mt-1 flex"> % produced by reforming of fossil fuel: <div className="font-bold px-1">95 %</div></div>
+          <div className="mt-1 flex"> Energy equivalent of the <div className="px-1"> {H2} </div> production: <div className="font-bold px-1">0.2 Gtoe/year</div>. For oil: <div className="font-bold px-1">4.4 Gt/year</div></div>
+          <div className="mt-1 flex"> World primary energy consumption: <div className="font-bold px-1">14,5 Gtoe/year</div></div>
+          <div className="font-bold text-l mt-2">Main current utilization of {H2}</div>
+          <div className="mt-1 flex"> Oil refining: <div className="font-bold px-1">44%</div></div>
+          <div className="mt-1 flex"> Ammonia for fertilizers: <div className="font-bold px-1">38%</div></div>
+          <div className="mt-1 flex"> Food industry: <div className="font-bold px-1">9%</div></div>
+          <div className="mt-1 flex"> Methanol, chemicals: <div className="font-bold px-1">8%</div></div>
+          <div className="mt-1 flex"> Space propulsion: <div className="font-bold px-1">1%</div></div>
+
+
+
+
+
+          <div className="font-bold text-xl mb-3 underline">Proton Exchange Membrane </div>
+          <div className="mt-2"> A proton exchange membrane is a semipermeable membrane designed to let protons go through but block reactants (for example {H2} and {O2}). It can be used as a fuel cell following this reaction: {reaction} </div>
+          <div className="mt-4">
+            <img src={pem} className="px-2 border-b-2" width="200" alt="Image" />
+            <div className="text-sm">source: wikipédia</div>
+          </div>
         </div>
       </div>
     </>
