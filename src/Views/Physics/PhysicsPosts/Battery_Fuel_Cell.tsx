10e9c8229f1ea240ea85fc5c3955a59b3c552839
Osgood <eloi.besnard@telecom-sudparis.eu>
2024-01-18 10:13:55 +0100

    factorisation of Tableau

--- src/Views/Physics/PhysicsPosts/Battery_Fuel_Cell.tsx
+++ src/Views/Physics/PhysicsPosts/Battery_Fuel_Cell.tsx
@@ -1,11 +1,8 @@
 import { HeaderCategory } from "../../../Single/HeaderCategory";
 import "katex/dist/katex.min.css";
 import Latex from "react-latex-next";
-import React from "react";
 import "katex/dist/katex.min.css";
-
-import "katex/dist/katex.min.css";
-
+import { Tableau } from "../../../Single/Tableau";
 const batteryData: (string)[][] = [
   [
     "LCO",
@@ -124,9 +121,7 @@ const fuelCellData: (string | JSX.Element | JSX.Element[])[][] = [[
   "Development",
 ]];
 
-
-
-const Battery_Fuel_Cell = ({ postID }: { postID: string }) => {
+const Battery_Fuel_Cell = () => {
   return (
     <>
       <div className="flex justify-center m-2">
@@ -136,128 +131,8 @@ const Battery_Fuel_Cell = ({ postID }: { postID: string }) => {
             date={"16/10/2023"}
             back={`/physics/`}
           />
-          <div className="my-20 justify-center">
-            <div className="font-bold text-xl mb-3 underline">Battery: Lithium-ion technologies</div>
-            <div className="overflow-x-auto">
-              <div className="grid grid-cols-14 gap-2 ">
-                <div className="font-bold text-1xl text-center w-28 col-start-1 col-end-3 col-span-2 ">
-                  Battery type{" "}
-                </div>
-                <div className="font-bold text-1xl text-center w-28  col-start-3 col-end-5 col-span-2">
-                  Cathode
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-5 col-end-7 col-span-2">
-                  Anode
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-7 col-end-9 col-span-2">
-                  Cell Voltage
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-9 col-end-10 col-span-1">
-                  Cost
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-10 col-end-11 col-span-1">
-                  Energy density
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-11 col-end-12 col-span-1">
-                  Cycle life
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-12 col-end-14 col-span-2">
-                  Discharge Rate
-                </div>
-              </div>
-              {batteryData.map(
-                (
-                  [text1, text2, text3, text4, text5, text6, text7, text8],
-                  index
-                ) => (
-                  <div className="grid grid-cols-14 gap-2" key={index}>
-                    <div className=" text-1xl text-center w-28 col-start-1 col-end-3 col-span-2 ">
-                      {text1}
-                    </div>
-                    <div className=" text-1xl text-center w-28  col-start-3 col-end-5 col-span-2">
-                      {text2}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-5 col-end-7 col-span-2">
-                      {text3}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-7 col-end-9 col-span-2">
-                      {text4}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-9 col-end-10 col-span-1">
-                      {text5}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-10 col-end-11 col-span-1">
-                      {text6}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-11 col-end-12 col-span-1">
-                      {text7}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-12 col-end-14 col-span-2">
-                      {text8}
-                    </div>
-                  </div>
-                )
-              )}
-            </div>
-          </div>
-          <div className="my-20 justify-center">
-            <div className="font-bold text-xl mb-3 underline">Main H2 fuel cell technology</div>
-            <div className="overflow-x-auto">
-              <div className="grid grid-cols-14 gap-2 ">
-                <div className="font-bold text-1xl text-center w-28 col-start-1 col-end-3 col-span-2 ">
-                  Type of Fuel cell{" "}
-                </div>
-                <div className="font-bold text-1xl text-center w-28  col-start-3 col-end-5 col-span-2">
-                  Electrolyte
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-5 col-end-7 col-span-2">
-                  T°C
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-7 col-end-9 col-span-2">
-                  Fuel
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-9 col-end-10 col-span-1">
-                  Efficiency
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-10 col-end-11 col-span-1">
-                  Applications
-                </div>
-                <div className="font-bold text-1xl text-center w-20 col-start-11 col-end-12 col-span-1">
-                  Maturity
-                </div>
-              </div>
-              {fuelCellData.map(
-                (
-                  [text1, text2, text3, text4, text5, text6, text7],
-                  index
-                ) => (
-                  <div className="grid grid-cols-14 gap-2" key={index}>
-                    <div className=" text-1xl text-center w-28 col-start-1 col-end-3 col-span-2 ">
-                      {text1}
-                    </div>
-                    <div className=" text-1xl text-center w-28  col-start-3 col-end-5 col-span-2">
-                      {text2}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-5 col-end-7 col-span-2">
-                      {text3}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-7 col-end-9 col-span-2">
-                      {text4}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-9 col-end-10 col-span-1">
-                      {text5}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-10 col-end-11 col-span-1">
-                      {text6}
-                    </div>
-                    <div className=" text-1xl text-center w-20 col-start-11 col-end-12 col-span-1">
-                      {text7}
-                    </div>
-                  </div>
-                )
-              )}
-            </div>
-          </div>
+          <Tableau content={batteryData} headers={["Battery type", "Cathode", "Anode", "Cell Voltage", "Cost", "Energy Density", "Cycle Life", "Discharge Rate"]} title={"Battery: Lithium-ion technologies"} />
+          <Tableau content={fuelCellData} headers={["Type", "Electrolyte", "Temp°C", "Fuel", "Efficiency", "Power", "Maturity"]} title={"Main H2 fuel cell technologies"} />
         </div>
       </div>
     </>
