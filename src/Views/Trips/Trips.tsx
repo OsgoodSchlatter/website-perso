5f2411e5cd3400cb4e98afec31a1ed29a84aaec2
Eloi BESNARD <eloi.besnard@ensta-paris.fr>
2024-10-06 18:13:18 +0200

    adding map in trips

--- src/Views/Trips/Trips.tsx
+++ src/Views/Trips/Trips.tsx
@@ -1,6 +1,3 @@
-import { Header } from "../../Single/Header";
-import { ListOfPostsHeader } from "../../Single/ListOfPostsHeader";
-
 import { useParams } from "react-router-dom";
 import { Annecy } from "./TripsPosts/Annecy";
 import { Biarritz } from "./TripsPosts/Biarritz";
@@ -27,6 +24,10 @@ import { Albi } from "./TripsPosts/Albi";
 import { BxHayAmst23 } from "./TripsPosts/BxHayAmst23";
 import { Bordeaux } from "./TripsPosts/Bordeaux";
 import { SweDen } from "./TripsPosts/SweDen";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Header } from "../../Single/Header";
+import L from "leaflet";
 
 export const blogPosts = {
   "1": {
@@ -245,11 +246,45 @@ export const TripsPosts = () => {
   return <Component title={post.title} date={post.date} />;
 };
 
+// SVG for red pin
+const PinSVG = (color: string) => `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+  <path fill="${color}" d="M12 2C8.14 2 5 5.14 5 9c0 4.53 4.58 10.74 6.39 12.96.34.41.9.41 1.24 0C14.42 19.74 19 13.53 19 9c0-3.86-3.14-7-7-7zm0 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/>
+</svg>
+`;
+
+const createCustomIcon = (color: string) => {
+  const pinSVG = PinSVG(color);
+  const pinIconUrl = `data:image/svg+xml;base64,${btoa(pinSVG)}`;
+
+  return new L.Icon({
+    iconUrl: pinIconUrl,
+    iconSize: [24, 24], // Size of the icon
+    iconAnchor: [12, 24], // Point of the icon which will correspond to marker's location
+    popupAnchor: [0, -24], // Point from which the popup should open relative to the iconAnchor
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize: [41, 41], // Size of the shadow
+  });
+};
+
 export const Trips = () => {
   return (
     <>
-      <ListOfPostsHeader topic="trips" blogPosts={blogPosts} trips={true} />
-
+      <Header name={"Trips"} posts={1} />
+      <div className="flex bg-white justify-center p-2 mt-4 rounded">
+        {/* @ts-ignore */}
+        <MapContainer center={[48.505, -3.5]} zoom={10} scrollWheelZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* @ts-ignore */}
+          <Marker position={[47.65, -2.7608]} icon={createCustomIcon("blue")}>
+            <Popup>
+              Vannes <br /> My hometown
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </>
   );
 };
