4f5a5c491b7144f226f81d9c6d4cc6ea0b462fe4
Eloi BESNARD <eloiposeidon@gmail.com>
2024-10-21 10:35:31 +0200

    contact section

--- src/Views/Recruiter/Recruiter.tsx
+++ src/Views/Recruiter/Recruiter.tsx
@@ -4,25 +4,12 @@ export const Recruiter = () => {
   return (
     <>
       <div className="flex justify-center ">
-        <div className="max-w-4xl">
-          <HeaderCategory
-            name={"How to contact / recruit me"}
-            date={"2023"}
-            back={`/home/`}
-          />
-
-          <div className="p-4 my-20 justify-center">
-
-            <div className="text-xl font-bold p-2 ">1- See my CV</div>
-            <div className="text-xl font-bold p-2 ">2- See my publications</div>
-            <div className="text-xl font-bold p-2 ">3- Explore my website</div>
-            <div className="text-xl font-bold p-2 ">4- Write me
-              <div className="pl-20 font-normal hover:none">tel: +33 7 83 67 76 20</div>
-              <div className="pl-20 font-normal hover:none">mail: eloiposeidon@gmail.com</div>
-              <div className="pl-20 font-normal hover:none">mail2: eloi.besnard@ensta-paris.fr</div>
-            </div>
+        <div className="md:w-1/2 p-4">
+          <div className="text-xl font-bold p-2 "> Write me
+            <div className=" font-normal hover:none">tel: +33 7 83 67 76 20</div>
+            <div className=" font-normal hover:none">mail: eloiposeidon@gmail.com</div>
+            <div className=" font-normal hover:none">mail2: eloi.besnard@ensta-paris.fr</div>
           </div>
-
         </div>
       </div>
     </>
