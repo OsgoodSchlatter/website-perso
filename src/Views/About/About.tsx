bf9e581847b2335fdb3d55f6dcfea07fe3ead36e
Eloi BESNARD <eloiposeidon@gmail.com>
2025-02-01 23:24:16 +0100

    about reworked

--- src/Views/About/About.tsx
+++ src/Views/About/About.tsx
@@ -4,32 +4,25 @@ import img1 from "./about_pic.jpg"
 const AboutContent = () => {
     return (
         <>
-            <div className="flex">
-                <div className="w-1/2 items-center">
-                    <div className="justify-center text-3xl font-bold ">
-                        Hi, I am Eloi.
-                    </div>
-                    <div className="text-lg ">
-                        I am a French citizen born in 2000 in France.
-                        <br />
-                        I work in nuclear physics and computer science.
-                        <br />
-                        My passions are making music, gliding sports and taking the train to go explore the world.
+            <div>
+                <div className="mb-4 flex justify-center items-center">
+                    <div className="text-center">
+                        <img src={img1} className="rounded mx-auto w-1/2" />
+                        <p>A picture of me in Salerno, Italy (2024)</p>
                     </div>
                 </div>
-                <div className="w-1/2">
-                    <img src={img1} className="rounded" />
 
-                    A picture of me in Salerno, Italy (2024)
-
+                <div className="text-3xl font-bold ">
+                    Hi, I am Eloi.
                 </div>
-
+                <div className="text-lg mb-4">
+                    I am a French citizen born in 2000 in France.
+                    <br />
+                    I work in nuclear physics and computer science.
+                    <br />
+                    My passions are making music, gliding sports and taking the train to go explore the world.
+                </div>
             </div>
-            <br />
-
-            <br />
-            <br />
-            <br />
 
         </>
     );
--- src/Views/Home/Data.tsx
+++ src/Views/Home/Data.tsx
@@ -253,7 +253,7 @@ export const blogPostsArray: BlogPostType[] = [
     {
         id: 1,
         date: "2024",
-        title: "Photo gallery",
+        title: "photo gallery",
         content_name: "Gallery",
         category: BlogCategory.Gallery,
         route: BlogRoute.Gallery,
