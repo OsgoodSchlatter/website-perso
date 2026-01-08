70d563b3c3789f926d0d99be4e914971b966fec2
Eloi BESNARD <eloiposeidon@gmail.com>
2026-01-08 10:07:08 +0100

    adding netlify secrt key env

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.SECRET_STRIPE_LIVE);
 
 exports.handler = async (event) => {
   const { priceId } = JSON.parse(event.body);
